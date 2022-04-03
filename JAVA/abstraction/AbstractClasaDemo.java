ef36818cca813e0381a6884483bda886d8e7d37d
Elijah Joseph <101247020+Elijah107@users.noreply.github.com>
2022-04-03 22:16:04 +0530

    Delete AbstractClasaDemo.java

--- JAVA/abstraction/AbstractClasaDemo.java
+++
@@ -1,9 +0,0 @@
-package abstraction;
-
-abstract class AbstractClasaDemo {            //abstract class  ....you cannot create an object
-
-	public static void main(String[] args) {
-     // AbstractClassDemo obj= new AbstractClassDemo
-		// abstract class ..= can't create an object
-	}
-}
